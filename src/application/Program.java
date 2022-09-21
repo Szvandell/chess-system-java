573e8cf772a2aad99389512b8fec0793167b7670
Szvandell <evandrovandell@gmail.com>
2022-09-21 16:37:23 -0300

    switichin player turn

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,7 +18,7 @@ public class Program {
 		while (true) {
 		try {
 		UI.clearScreen();
-		UI.PrintBoard(chessMatch.getPieces());
+		UI.printMatch(chessMatch);
 		System.out.println();
 		System.out.println("Source: ");
 		ChessPosition source =UI.readChessPosition(sc);
